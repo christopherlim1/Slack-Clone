c5b175e4a1d74f0329dbae43eb060399a72ef721
cblim <cblim@ucsc.edu>
2021-12-09 18:27:41 -0800

    works

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 const app = require('./app.js');
 
-const port = process.env.PORT || 3500;
+const port = process.env.PORT || 3010;
 
-app.listen(3500, () => {
+app.listen(3010, () => {
   console.log(`Server Running on port ${port}`);
   console.log(`API Testing UI: http://localhost:${port}/v0/api-docs/`);
 });
